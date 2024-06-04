1f3854a806b2a42f02ad419d2d09cc88e5672195
damienmillet <contact@damien-millet.dev>
2024-06-04 13:59:28 +0200

    Refactor Api headers and remove unnecessary console.log statements

--- lib/api.ts
+++ lib/api.ts
@@ -19,6 +19,7 @@ class Api {
     options?: RequestInit,
   ) {
     options = { headers: this.headers, ...options };
+    console.log(options);
     const req = fetch(url, options);
     const res = await req;
 
