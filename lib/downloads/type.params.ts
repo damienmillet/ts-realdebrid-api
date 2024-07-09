c0397b63488f0525ba407a8e0f9bf08c0e44436c
damienmillet <contact@damien-millet.dev>
2024-07-09 14:15:19 +0200

    Remove unused downloads module and update index.ts

--- lib/downloads/type.params.ts
+++
@@ -1,5 +0,0 @@
-export type downloadsParams = {
-  limit?: string | string[] | number;
-  page?: string | string[] | number;
-  offset?: string | string[] | number;
-};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import * as User from "./services/user";
 import * as Time from "./services/time";
+import * as Downloads from "./services/downloads";
+import * as Hosts from "./services/hosts";
+import * as Settings from "./services/settings";
+import * as Streaming from "./services/streaming";
+import * as Torrents from "./services/torrents";
+import * as Traffic from "./services/traffic";
+import * as Unrestrict from "./services/unrestrict";
 
 const Index = {
   User,
   Time,
+  Downloads,
+  Hosts,
+  Settings,
+  Streaming,
+  Torrents,
+  Traffic,
+  Unrestrict,
 };
 
 export default Index;
