0e1a239d07d8fcc368ffc653445ea826eacd016a
damienmillet <contact@damien-millet.dev>
2024-07-28 16:49:56 +0200

    chore: Refactor fetchAPI function to handle errors and success responses consistently

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -2,42 +2,54 @@ export async function fetchAPI(
   path: string,
   options: RequestInit = {},
 ): Promise<any> {
-  const url = new URL("https://api.real-debrid.com/rest/1.0/" + path);
+  try {
+    const url = new URL(`https://api.real-debrid.com/rest/1.0/${path}`);
 
-  options.headers = {
-    ...options?.headers,
-    Authorization: `Bearer ${process.env.REALDEBRID_API}`,
-    accept: "application/json",
-  };
+    options.headers = {
+      ...options.headers,
+      Authorization: `Bearer ${process.env.REALDEBRID_API}`,
+      accept: "application/json",
+    };
 
-  const response = await fetch(url.toString(), {
-    ...options,
-    cache: "no-cache",
+    const response = await fetch(url.toString(), {
+      ...options,
+      cache: "no-cache",
+    });
+
+    if (!response.ok) return handleError(response.statusText, response.status);
+
+    if (response.status === 204) {
+      return handleSuccess(undefined, 204);
+    }
+
+    if (response.url.includes("time") || response.url.includes("time/iso")) {
+      const time = await response.text();
+      return handleSuccess(time);
+    }
+
+    const data = await response.json();
+    const { error_code, error } = data;
+
+    if (error_code || error) {
+      return { success: false, error: { code: error_code, message: error } };
+    }
+
+    return { success: true, ...(data && { data: data }) };
+  } catch (error) {
+    if (error instanceof Error) {
+      return handleError(error.message);
+    }
+  }
+}
+
+export function handleError(message: string, code = 500) {
+  return Response.json({ success: false, error: { code, message } }, {
+    status: code,
   });
+}
 
-  if (!response.ok) {
-    return {
-      success: false,
-      error: { code: response.status, message: response.statusText },
-    };
-  }
-
-  if (response.status === 204) {
-    return { success: true };
-  }
-
-  if (response.url.includes("time") || response.url.includes("time/iso")) {
-    return { success: true, data: { time: await response.text() } };
-  }
-
-  const data = await response.json();
-
-  if (data.error_code || data.error) {
-    return {
-      success: false,
-      error: { code: data.error_code, message: data.error },
-    };
-  }
-
-  return { success: true, ...(data && { data: data }) };
+export function handleSuccess(data?: any, code = 200) {
+  return Response.json({ success: true, ...(data && { data: data }) }, {
+    status: code,
+  });
 }
